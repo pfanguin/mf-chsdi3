c2038bff9b872bedcf896af01ab19c51f7fd4004
Martin Kuenzi <39826573+ltkum@users.noreply.github.com>
2018-12-12 14:22:57 +0100

    faulty line found, I think

--- chsdi/lib/validation/identify.py
+++ chsdi/lib/validation/identify.py
@@ -143,6 +143,7 @@ class IdentifyServiceValidation(BaseFeaturesValidation):
                     print("hey")
                     print(esrijson.loads(value.replace("'", "\"")))
                     print(esrijson.loads(value.replace("'", "\""))['spatialReference']['wkid'])
+                    print("lock here")
                     self._geometry = esrijson.to_shape(esrijson.loads(value))
                     print(self.geometry)
                     print(type(self._geometry))
